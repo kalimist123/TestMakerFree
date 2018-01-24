ae80f42a18f839ce97ea9796a1ffcb2d49baf0e4
Michael Lennon <kali.mist@gmail.com>
2018-01-24 12:25:13 +0000

    refactor quiz-component

--- TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-list.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-list.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Inject } from "@angular/core";
+﻿import { Component, Inject, Input, OnInit  } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
@@ -7,17 +7,43 @@ import { HttpClient } from "@angular/common/http";
     styleUrls: ['./quiz-list.component.css']
 })
 
-export class QuizListComponent {
+export class QuizListComponent implements OnInit {
+    
+    @Input() class: string;
     title: string;
     selectedQuiz: Quiz;
     quizzes: Quiz[];
+    
 
-    constructor(http: HttpClient,
-        @Inject('BASE_URL') baseUrl: string) {
-        this.title = "Latest Quizzes";
-        var url = baseUrl + "api/quiz/Latest/";
+    constructor(private http: HttpClient,
+        @Inject('BASE_URL') private baseUrl: string) {
+        
+    }
 
-        http.get<Quiz[]>(url).subscribe(result => {
+    ngOnInit(): void {
+        console.log("QuizListComponent " +
+            " instantiated with the following class: "
+            + this.class);
+
+        var url = this.baseUrl + "api/quiz/";
+
+        switch (this.class) {
+        case "latest":
+        default:
+            this.title = "Latest Quizzes";
+            url += "Latest/";
+            break;
+        case "byTitle":
+            this.title = "Quizzes by Title";
+            url += "ByTitle/";
+            break;
+        case "random":
+            this.title = "Random Quizzes";
+            url += "Random/";
+            break;
+        }
+
+        this.http.get<Quiz[]>(url).subscribe(result => {
             this.quizzes = result;
         }, error => console.error(error));
     }
